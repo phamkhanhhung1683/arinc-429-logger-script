1c215ea9bf4c0eb770403c3351aafc2e96a475e2
phamkhanhhung <phamkhanhhung1683@gmail.com>
2026-03-09 15:36:55 +0700

    refactor

--- scripts/main.py
+++ scripts/main.py
@@ -1,32 +1,10 @@
 import argparse
 import socket
-import sqlite3
 import time
 
+from init_db import init_db
 from load_message import load_message
 from transform_message import transform_message
-
-
-def init_db():
-    conn = sqlite3.connect("arinc_429_log.db")
-    conn.execute('PRAGMA journal_mode=WAL;')
-
-    cursor = conn.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS arinc_429_messages (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            timestamp DATETIME,
-            channel INTEGER,
-            raw_message TEXT,
-            raw_label_field TEXT,
-            raw_data_field TEXT,
-            raw_ssm_field TEXT,
-            raw_parity_field TEXT,
-            processed_label_field TEXT
-        )
-    ''')
-
-    return conn
 
 
 def decode_arinc_429_word(raw_str):
