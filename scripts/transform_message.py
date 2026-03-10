6378d2fa2a59ca3f44bc4e6752f31c1e46c483d5
phamkhanhhung <phamkhanhhung1683@gmail.com>
2026-03-10 14:44:46 +0700

    add processed_data of 210 and 211 message

--- scripts/transform_message.py
+++ scripts/transform_message.py
@@ -1,3 +1,18 @@
+from decimal import Decimal, getcontext
+
+
+getcontext().prec = 40
+
+
+def binary_to_decimal(bin_str):
+    try:
+        decimal_val = int(bin_str, 2)
+        return decimal_val
+    except ValueError as e:
+        print(f"[ERROR] Value error: {e}")
+        return None
+
+
 def transform_message(raw_message):
     if not raw_message:
         return None
@@ -16,6 +31,16 @@ def transform_message(raw_message):
     else:
         message_group = None
 
+    processed_data = None
+    match processed_label:
+        case "210" | "211":
+            raw_val = binary_to_decimal(raw_message['fields']['data'])
+            if raw_val is not None:
+                scale = Decimal(90) / (Decimal(2) ** 20)
+                processed_data = (scale * Decimal(raw_val)).quantize(Decimal("1e-30"))
+                processed_data = str(processed_data)
+
+
     processed_message = {
         "channel": raw_message['channel'],
         "raw_message": raw_message['binary'],
@@ -26,18 +51,10 @@ def transform_message(raw_message):
             "parity": raw_message['fields']['parity'],
         },
         "processed_fields": {
-            "label": processed_label
+            "label": processed_label,
+            "data": processed_data
         },
         "message_group": message_group
     }
 
     return processed_message
-
-
-def binary_to_decimal(bin_str):
-    try:
-        decimal_val = int(bin_str, 2)
-        return decimal_val
-    except ValueError as e:
-        print(f"[ERROR] Value error: {e}")
-        return None
