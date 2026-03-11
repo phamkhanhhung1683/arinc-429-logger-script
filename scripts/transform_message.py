f3630804a0d8c8aa08fa44759b6c35a6c11993ba
phamkhanhhung <phamkhanhhung1683@gmail.com>
2026-03-11 09:28:12 +0700

    add case to process data field

--- scripts/transform_message.py
+++ scripts/transform_message.py
@@ -36,10 +36,37 @@ def transform_message(raw_message):
         case "210" | "211":
             raw_val = binary_to_decimal(raw_message['fields']['data'])
             if raw_val is not None:
-                scale = Decimal(90) / (Decimal(2) ** 20)
+                scale = Decimal("90") / (Decimal("2") ** 20)
                 processed_data = (scale * Decimal(raw_val)).normalize()
                 processed_data = str(processed_data)
 
+        case "223":
+            raw_val = binary_to_decimal(raw_message['fields']['data'])
+            if raw_val is not None:
+                scale = Decimal("65536") / (Decimal("2") ** 20)
+                processed_data = (scale * Decimal(raw_val)).normalize()
+                processed_data = str(processed_data)
+
+        case "266" | "267" | "264":
+            raw_val = binary_to_decimal(raw_message['fields']['data'])
+            if raw_val is not None:
+                scale = Decimal("3034.3168") / (Decimal("2") ** 20)
+                processed_data = (scale * Decimal(raw_val)).normalize()
+                processed_data = str(processed_data)
+
+        case "127":
+            raw_val = binary_to_decimal(raw_message['fields']['data'])
+            if raw_val is not None:
+                scale = Decimal("104857.6") / (Decimal("2") ** 20)
+                processed_data = (scale * Decimal(raw_val)).normalize()
+                processed_data = str(processed_data)
+
+        case "320":
+            raw_val = binary_to_decimal(raw_message['fields']['data'][:15])
+            if raw_val is not None:
+                scale = Decimal("16384") / (Decimal("2") ** 14)
+                processed_data = (scale * Decimal(raw_val)).normalize()
+                processed_data = str(processed_data)
 
     processed_message = {
         "channel": raw_message['channel'],
